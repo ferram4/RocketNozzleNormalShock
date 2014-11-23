5ffb0c494106bdd039817a018c866e3606902a50
ferram4 <ferram40@gmail.com>
2014-11-23 01:37:51 -0500

    It grows

--- CalculateRocketNozzleNormalShock/Program.cs
+++ CalculateRocketNozzleNormalShock/Program.cs
@@ -7,11 +7,32 @@ namespace CalculateRocketNozzleNormalShock
 {
     class Program
     {
-        static MachAreaRelation machAreaRelation;
+        static RocketNozzleNormalShockSim currentSim;
         static void Main()
         {
-            System.Console.WriteLine("Prepare For Normal Shock Calculations...");
-            machAreaRelation = new MachAreaRelation(1.4, 40, 0.1);
+            System.Console.WriteLine("Welcome to Rocket Nozzle Normal Shock Location Calculator");
+            System.Console.WriteLine("");
+            bool cont = true;
+            while (cont)
+            {
+                currentSim = new RocketNozzleNormalShockSim();
+
+                cont = Continue();
+            }
+        }
+
+        static bool Continue()
+        {
+            char ch;
+            do
+            {
+                System.Console.Write("Continue? (Y/N)");
+                int input = System.Console.Read();
+                ch = Convert.ToChar(input);
+                System.Console.WriteLine("");
+            } while (ch != 'y' && ch != 'Y' && ch != 'n' && ch != 'N');
+
+            return (ch == 'y' || ch == 'Y');
         }
     }
 }
---
+++ CalculateRocketNozzleNormalShock/RocketNozzleNormalShockSim.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateRocketNozzleNormalShock
+{
+    class RocketNozzleNormalShockSim
+    {
+        MachAreaRelation machAreaRelation;
+        double exitAreaRatio;
+        double gamma;
+
+        double chamberPressure;
+        double backPressure;
+        //machAreaRelation = new MachAreaRelation(1.4, 40, 0.1);
+
+        public RocketNozzleNormalShockSim()
+        {
+            GetDataEntry();
+            
+
+        }
+
+        private void GetDataEntry()
+        {
+
+            System.Console.WriteLine("Beginning new nozzle sim...");
+            System.Console.WriteLine("");
+
+            System.Console.WriteLine("Please enter the following information:");
+            System.Console.WriteLine("");
+            exitAreaRatio = GetDoubleData("Nozzle Area Ratio: ");
+            gamma = GetDoubleData("Exhaust Ratio of Specific Heats (gamma): ");
+            System.Console.WriteLine("");
+            chamberPressure = GetDoubleData("Engine Chamber Pressure (Pa): ");
+            backPressure = GetDoubleData("Ambient Pressure (Pa): ");
+            System.Console.WriteLine("");
+
+            machAreaRelation = new MachAreaRelation(1.4, 40, 0.1);
+            System.Console.WriteLine("");
+
+        }
+        private double GetDoubleData(string dataToRequest)
+        {
+            string tmp;
+            double result;
+
+            bool correctEntry = true;
+            do
+            {
+                System.Console.Write(dataToRequest);
+                tmp = System.Console.ReadLine();
+
+                correctEntry = correctEntry = double.TryParse(tmp, out result);
+                if (!correctEntry)
+                {
+                    System.Console.WriteLine("Error parsing; please input a valid number");
+                    System.Console.WriteLine("");
+                }
+
+            } while (!correctEntry);
+
+            return result;
+        }
+    }
+}
